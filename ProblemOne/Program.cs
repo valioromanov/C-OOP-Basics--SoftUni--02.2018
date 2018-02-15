8723234597f616755a8272655f412969f59fe204
valioromanov <valioromanov@gmail.com>
2018-02-15 16:15:04 +0200

    Add files via upload

---
+++ ProblemOne/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ProblemOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Family family = new Family();
+            int input = int.Parse(Console.ReadLine());
+            for (int i = 0; i < input; i++)    
+            {
+                Person person = new Person();
+                var personInfo = Console.ReadLine().Split(" ");
+                person.Name = personInfo[0];
+                person.Age = int.Parse(personInfo[1]);
+                family.AddMember(person);
+            }
+
+            Person oldestPerson = family.GetOldestMember();
+            Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+        }
+    }
+}
